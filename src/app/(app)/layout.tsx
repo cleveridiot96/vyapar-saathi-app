ba8285fb7e6d353b5a7901e20d067130d4dcaee2
Mira Parmar <miramasala1@gmail.com>
2026-01-04 10:13:01 +0000

    1 of 1 error Next.js (15.0.7) is outdated (learn more) (Turbopack)
    Unha

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,11 +3,14 @@
 import React from 'react';
 import { AppShell } from '@/components/layout/AppShell';
 import { AppHeader } from '@/components/layout/AppHeader';
+import { PasswordProvider } from '@/contexts/PasswordContext';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AppShell header={<AppHeader />}>
-        {children}
-    </AppShell>
+    <PasswordProvider>
+      <AppShell header={<AppHeader />}>
+          {children}
+      </AppShell>
+    </PasswordProvider>
   );
 }
